5c868ff75a365b871e937891a48859c11b002053
javiemgz <javiemgz@gmail.com>
2020-06-14 20:03:27 -0300

    make it betterw

--- src/app/services/configurations.ts
+++ src/app/services/configurations.ts
@@ -1,3 +1,3 @@
-//export const REST_SERVER_URL = 'http://localhost:16000'
+export const REST_SERVER_URL = 'http://localhost:16000'
 
-export const REST_SERVER_URL = 'http://186.137.65.8:16000'
+//export const REST_SERVER_URL = 'http://186.137.65.8:16000'
