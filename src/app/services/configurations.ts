4c8b92e2c4b737fb383ebe8033d66511a18e5411
javiemgz <javiemgz@gmail.com>
2020-06-20 00:14:06 -0300

    fixed delte

--- src/app/services/configurations.ts
+++ src/app/services/configurations.ts
@@ -1,3 +1,12 @@
 export const REST_SERVER_URL = 'https://tfm-back.herokuapp.com'
 
 //export const REST_SERVER_URL = 'http://186.137.65.8:16000'
+
+
+//TODO EDITAR....
+//BOTON delete
+
+//nombre es decripcion tablas
+
+//botos tablas :()
+//usuarios carga...
